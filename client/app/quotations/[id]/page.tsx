9002bf4b7c6b95b1d9b06b4197aacd4bb2f7698b
Abaaza <abaza90@hotmail.com>
2025-06-13 17:21:31 +0300

    Fix quotation detail page export

--- client/app/quotations/[id]/page.tsx
+++ client/app/quotations/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { DashboardLayout } from "@/components/dashboard-layout"
 import { QuotationDetail } from "@/components/quotation-detail"
 
-export default async function QuotationDetailPage({
+export default function QuotationDetailPage({
   params,
 }: {
   params: { id: string }
 }) {
-  const { id } = await params
+  const { id } = params
   return (
     <DashboardLayout>
       <QuotationDetail quotationId={id} />
